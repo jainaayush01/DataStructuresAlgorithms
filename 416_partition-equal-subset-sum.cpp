be995d56c5a5f7ce4354acb574b57c4b02ba8471
Aayush Jain <imjainaayush@gmail.com>
2021-05-21 18:45:32 +0530

    2 dp problems

---
+++ 416_partition-equal-subset-sum.cpp
@@ -0,0 +1,53 @@
+// 416 https://leetcode.com/problems/partition-equal-subset-sum/
+
+class Solution {
+public:
+    bool isSubsetSum(int N, vector<int> arr, int sum){
+        // Dynamic Programming
+        // 01 knapsack problem
+        // choice: select the number or do not select the number
+        int dp[N+1][sum+1]; 
+        // dp[i][j] = displays is sum=j possible from 
+        // subset of the first i elements of arr
+        
+        // iniitalization: 
+        for(int j = 1; j <= sum; ++j) {
+            dp[0][j] = 0; // sum=1...sum not possible from first 0 elements
+        }
+        for(int i = 0; i <= N; ++i) {
+            dp[i][0] = 1; // sum=0 possible for first i=0...N by using empty subset
+        }
+        
+        for(int i = 1; i <= N; ++i) {
+            for(int j = 1; j <= sum; ++j) {
+                if(arr[i-1] <= j) { // check if arr[i-1] is less than sum
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                    // if sum was possible from i-1 elements 
+                    // if not then if sum=j-arr[i-1] was possible from 0-1 elements
+                }
+                else { // if greater than the curr ans = previous ans
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        // for(int i = 0; i <= N; ++i) {
+        //     for(int j = 0; j <= sum; ++j) {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        return dp[N][sum];
+    }
+    bool canPartition(vector<int>& nums) {
+        int total = 0, n = nums.size();
+        for(int i = 0; i < n; ++i) {
+            total += nums[i];
+        }
+        if(total%2) {
+            return false;
+        }
+        int sum = total/2;
+        return isSubsetSum(n, nums, sum);
+    }
+};
