baf377da060fa403ee1fa13324be35da4e3c8436
Aayush Jain <imjainaayush@gmail.com>
2021-05-25 16:27:49 +0530

    restructured

--- DP/01_Knapsack/CountOfSubsetsSumWithDiff.cpp
+++ DP/01_Knapsack/CountOfSubsetsSumWithDiff.cpp
@@ -1,6 +1,3 @@
-#include<bits/stdc++.h> 
-using namespace std; 
-
 class Solution{   
 public:
     int countSubsetSum(int N, int arr[], int sum){
--- DP/01_Knapsack/SubsetSumProblem.cpp
+++ DP/01_Knapsack/SubsetSumProblem.cpp
@@ -1,9 +1,6 @@
-#include<bits/stdc++.h> 
-using namespace std; 
-
 class Solution{   
 public:
-    bool countSubsetSum(int N, int arr[], int sum){
+    bool isSubsetSum(int N, int arr[], int sum){
         // Dynamic Programming
         // 01 knapsack problem
         // choice: select the number or do not select the number
@@ -13,29 +10,30 @@ public:
         
         // iniitalization: 
         for(int j = 1; j <= sum; ++j) {
-            dp[0][j] = 0; 
-            // sum=1...sum not possible from first 0 elements
+            dp[0][j] = 0; // sum=1...sum not possible from first 0 elements
         }
         for(int i = 0; i <= N; ++i) {
-            dp[i][0] = 1; 
-            // sum=0 possible for first i=0...N by using empty subset
+            dp[i][0] = 1; // sum=0 possible for first i=0...N by using empty subset
         }
         
         for(int i = 1; i <= N; ++i) {
             for(int j = 1; j <= sum; ++j) {
                 if(arr[i-1] <= j) { // check if arr[i-1] is less than sum
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]]; 
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
                     // if sum was possible from i-1 elements 
                     // if not then if sum=j-arr[i-1] was possible from 0-1 elements
-                    // dp[i-1][j] tells if the subset was formed without arr[i-1] or not
-                    // so basially dp[N][sum] tells us by going through all the subsets
                 }
-                else { 
-                    // if greater than the curr ans = previous ans
+                else { // if greater than the curr ans = previous ans
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
+        // for(int i = 0; i <= N; ++i) {
+        //     for(int j = 0; j <= sum; ++j) {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
         
         return dp[N][sum];
     }
