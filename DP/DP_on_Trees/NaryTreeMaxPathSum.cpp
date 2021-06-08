9fa5e37e4a123e3b7660e140efc2683444850f76
Aayush Jain <imjainaayush@gmail.com>
2021-06-08 21:06:40 +0530

    nary max path sum

--- DP/DP_on_Trees/NaryTreeMaxPathSum.cpp
+++ DP/DP_on_Trees/NaryTreeMaxPathSum.cpp
@@ -2,24 +2,32 @@ class Solution {
 public:
     int solve(Node* root, int &res) {
         if(root == NULL) {
-            return -2001;
+            return 0;
         }
         int leftSum = solve(root->left, res);
         int rightSum = solve(root->right, res);
         
-        int temp = max(leftSum, rightSum) + root->data;
-        if(root->left == NULL && root->right == NULL) {
-            temp = root->data;
-            return temp;
+        int temp = root->data;
+        if(root->left && root->right) {
+            temp += max(leftSum, rightSum);
+            int ans = leftSum + rightSum + root->data;
+            res = max(res, ans);
         }
-        int ans = leftSum + rightSum + root->data;
-        res = max(res, ans);
+        else if(!root->left) {
+            temp += rightSum;
+        }
+        else if(!root->right) {
+            temp += leftSum;
+        }
         
         return temp;
     }
     int maxPathSum(Node* root) {
-        int res = -1001;
-        solve(root, res);
+        int res = INT_MIN;
+        int ret = solve(root, res);
+        if(res == INT_MIN) {
+            return ret;
+        }
         return res;
     }
 };
